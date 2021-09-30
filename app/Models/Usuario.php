96a2e10e981e8a722442f83689029169a0393f30
Janderson <janderson.rocha97@gmail.com>
2021-09-30 00:29:31 -0300

    Atualização model Usuário

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -14,7 +14,7 @@ class Usuario extends Model
     protected $returnType           = 'array';
     protected $useSoftDeletes       = false;
     protected $protectFields        = true;
-    protected $allowedFields        = ['id', 'nome', 'nivel'];
+    protected $allowedFields        = ['id', 'nome', 'password', 'nivel'];
 
     // Dates
     protected $useTimestamps        = false;
@@ -26,12 +26,19 @@ class Usuario extends Model
     // Validation
     protected $validationRules      = [
         'id' => 'required|is_unique[cire_backbone_usuarios.id]',
-
+        'nome' => 'required',
+        'nivel' => 'required',
     ];
     protected $validationMessages   = [
         'id' => [
             'required' => 'O campo RE é de preenchimento obrigatório',
             'is_unique' => 'Já consta usuário cadastrado com o RE informado',
+        ],
+        'nome' => [
+            'required' => 'O campo nome é de preenchimento obrigatório',
+        ],
+        'nivel' => [
+            'required' => 'O campo nível é de preenchimento obrigatório',
         ],
     ];
     protected $skipValidation       = false;
