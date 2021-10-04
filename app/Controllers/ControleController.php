b3d1aaac9e2668703353ee1c98edfe9623bc6223
Janderson <janderson.rocha97@gmail.com>
2021-10-04 18:35:48 -0300

    Insert carimbo escalonamento urgente (controle)

--- app/Controllers/ControleController.php
+++ app/Controllers/ControleController.php
@@ -64,29 +64,8 @@ class ControleController extends BaseController
 
         $nome_usuario = session()->get('nome');
 
-        /*
-        $rules = [
-            'numero_ta' => 'required|integer',
-            'nome_eps' => 'required',
-        ];
-
-        $feedback = [
-            'numero_ta' => [
-                'required' => 'Informar o TA.',
-                'integer' => 'O TA deve possuir apenas números.',
-            ],
-            'nome_eps' => [
-                'required' => 'Informar a EPS.',
-            ],
-        ];
-
-        $this->validate($rules, $feedback);
-        */
-
         //Criação do carimbo para insert no banco e retorno ao usuário.
         $response['carimbo'] = "xxxxxxxxxxxxxxx Escalonamento CRISE " . $this->request->getVar('tipo_carimbo') . " " . "xxxxxxxxxxxxxx\n----------------------------------------------------\nCIRE ATENTO\nControl Desk: " . $this->request->getVar('nome_control_desk_um_cire_atento') . " " . $this->request->getVar('forma_contato_control_desk_um_cire_atento') . "\nSupervisor(a): " . $this->request->getVar('nome_supervisao_cire_atento') . " " . $this->request->getVar('forma_contato_supervisao_cire_atento') . "\nCoordenador(a): " . $this->request->getVar('nome_coordenacao_cire_atento') . " " . $this->request->getVar('forma_contato_coordenacao_cire_atento') . "\nGestor(a): " . $this->request->getVar('nome_gestao_cire_atento') . " " . $this->request->getVar('forma_contato_gestao_cire_atento') . "\n----------------------------------------------------\nEPS (" . $this->request->getVar('nome_eps') . ")\nCoordenador(a): " . $this->request->getVar('nome_coordenacao_eps') . " " . $this->request->getVar('horario_contato_coordenacao_eps') . " " . $this->request->getVar('forma_contato_coordenacao_eps') . "\nGerente: " . $this->request->getVar('nome_gerente_eps') . " " . $this->request->getVar('horario_contato_gerente_eps')  . " " . $this->request->getVar('forma_contato_gerente_eps') . "\n----------------------------------------------------\nREDE EXTERNA\nCoordenador(a): " . $this->request->getVar('nome_coordenacao_rede_externa') . " " . $this->request->getVar('horario_contato_coordenacao_rede_externa') . " " . $this->request->getVar('forma_contato_coordenacao_rede_externa') . "\nGerente Seção: " . $this->request->getVar('nome_gerente_secao_rede_externa') . " " . $this->request->getVar('horario_contato_gerente_secao_rede_externa') . " " . $this->request->getVar('forma_contato_gerente_secao_rede_externa') . "\nGerente Divisão: " . $this->request->getVar('nome_gerente_divisao_rede_externa') . " " . $this->request->getVar('horario_contato_gerente_divisao_rede_externa') . " " . $this->request->getVar('forma_contato_gerente_divisao_rede_externa') . "\nDiretora(a): " . $this->request->getVar('nome_direcao_rede_externa') . " " . $this->request->getVar('horario_contato_direcao_rede_externa') . " " . $this->request->getVar('forma_contato_direcao_rede_externa') . "\n----------------------------------------------------\nCIRE VIVO\nGestor(a): " . $this->request->getVar('nome_gestao_cire_vivo') . " " . $this->request->getVar('horario_contato_gestao_cire_vivo') . " " . $this->request->getVar('forma_contato_gestao_cire_vivo') . "\nCoordenador(a): " . $this->request->getVar('nome_coordenacao_cire_vivo') . " " . $this->request->getVar('horario_contato_coordenacao_cire_vivo') . " " . $this->request->getVar('forma_contato_coordenacao_cire_vivo') . "\nGerente: " . $this->request->getVar('nome_gerente_cire_vivo') . " " . $this->request->getVar('horario_contato_gerente_cire_vivo') . " " . $this->request->getVar('forma_contato_gerente_cire_vivo') . "\nGerente Divisão: " . $this->request->getVar('nome_gerente_divisao_cire_vivo') . " " . $this->request->getVar('horario_contato_gerente_divisao_cire_vivo') . " " . $this->request->getVar('forma_contato_gerente_divisao_cire_vivo') . "\n----------------------------------------------------\nAnalista Cire: " . $nome_usuario . "\nxxxxxxxxxxxxxxx Escalonamento CRISE " . $this->request->getVar('tipo_carimbo') . " " . "xxxxxxxxxxxxxx";
-
-        $response['mensagem'] = "Carimbo gerado com sucesso.";
 
         //Atribuindo os valores
         $this->atividade->save([
@@ -94,6 +73,24 @@ class ControleController extends BaseController
             "data_hora" => date("Y-m-d H:i:s"),
             "numero_ta" => $this->request->getVar('numero_ta'),
             "tipo_atividade_id" => 1,
+            "carimbo" => $response['carimbo']
+        ]);
+
+        return $this->response->setJSON($response);
+    }
+
+    public function insertCarimboUrgente()
+    {
+        $nome_usuario = session()->get('nome');
+
+        $response['carimbo'] = "xxxxxxxxxxxxxxx Escalonamento " . $this->request->getVar('escalonamento_horas') . " horas xxxxxxxxxxxxxx\n----------------------------------------------------\nVIVO REDE\nN1: " . $this->request->getVar('n1_vivo_rede') . " " . $this->request->getVar('horario_contato_n1_vivo_rede') . " " . $this->request->getVar('forma_contato_n1_vivo_rede') . "\nN2: " . $this->request->getVar('n2_vivo_rede') . " " . $this->request->getVar('horario_contato_n2_vivo_rede') . " " . $this->request->getVar('forma_contato_n2_vivo_rede') . "\nN3: " . $this->request->getVar('n3_vivo_rede') . " " . $this->request->getVar('horario_contato_n3_vivo_rede') . " " . $this->request->getVar('forma_contato_n3_vivo_rede') . "\nN4: " . $this->request->getVar('n4_vivo_rede') . " " . $this->request->getVar('horario_contato_n4_vivo_rede') . " " . $this->request->getVar('forma_contato_n4_vivo_rede') .  "\nN5: " . $this->request->getVar('n5_vivo_rede') . " " . $this->request->getVar('horario_contato_n5_vivo_rede') . " " . $this->request->getVar('forma_contato_n5_vivo_rede') . "\n----------------------------------------------------\nVIVO CIRE \nN1: " . $this->request->getVar('n1_vivo_cire') . " " . $this->request->getVar('horario_contato_n1_vivo_cire') . " " . $this->request->getVar('forma_contato_n1_vivo_cire') . "\nN2: " . $this->request->getVar('n2_vivo_cire') . " " . $this->request->getVar('horario_contato_n2_vivo_cire') . " " . $this->request->getVar('forma_contato_n2_vivo_cire') . "\nN3: " . "" . $this->request->getVar('n3_vivo_cire') . " " . $this->request->getVar('horario_contato_n3_vivo_cire') . " " . $this->request->getVar('forma_contato_n3_vivo_cire') . "\nN4: " . "" . $this->request->getVar('n4_vivo_cire') . " " . $this->request->getVar('horario_contato_n4_vivo_cire') . " " . $this->request->getVar('forma_contato_n4_vivo_cire') . "\nN5: " . "" . $this->request->getVar('n5_vivo_cire') . " " . $this->request->getVar('horario_contato_n5_vivo_cire') .  " " . $this->request->getVar('forma_contato_n5_vivo_cire') . "\n----------------------------------------------------\nEPS " . $this->request->getVar('nome_eps') . "\nN1: " . $this->request->getVar('n1_eps') . " " . $this->request->getVar('horario_contato_n1_eps') . " " . $this->request->getVar('forma_contato_n1_eps') . "\nN2: " . $this->request->getVar('n2_eps') . " " . $this->request->getVar('horario_contato_n2_eps') . " " . $this->request->getVar('forma_contato_n2_eps') .  "\n----------------------------------------------------\nAnalista Cire: " . $nome_usuario . "\nxxxxxxxxxxxxxxx Escalonamento " . $this->request->getVar('escalonamento_horas') . " horas xxxxxxxxxxxxxx";
+
+        //Atribuindo os valores
+        $this->atividade->save([
+            "usuario_id" => session()->get('id'),
+            "data_hora" => date("Y-m-d H:i:s"),
+            "numero_ta" => $this->request->getVar('numero_ta'),
+            "tipo_atividade_id" => 2,
             "carimbo" => $response['carimbo']
         ]);
 
--- app/Views/app/carimbos/forms/controle/escalonamento_urgente.php
+++ app/Views/app/carimbos/forms/controle/escalonamento_urgente.php
@@ -1,1 +1,179 @@
-Form escalonamento urgente
+<div id="form_controle_escalonamento_urgente" class="my-3">
+
+    <b>Escalonamento URGENTE</b>
+    <form method="POST" id="carimbo_form" action="#">
+        <?= csrf_field() ?>
+        <div class="form-inline">
+            <input type="text" name="numero_ta" class="form-control" placeholder="Digite o TA" required>
+            <select class="custom-select my-1 mr-sm-2 ml-md-1" name="nome_eps" required>
+                <option value="" disabled selected>Selecione a EPS</option>
+                <?php foreach ($contratadas as $contratada) { ?>
+                    <option value="<?= $contratada['nome'] ?>"><?= $contratada['nome'] ?></option>
+                <?php } ?>
+            </select>
+            <input type="text" name="escalonamento_horas" class="form-control" placeholder="Escalonamento xx horas" required>
+        </div>
+
+        <div class="my-2">
+            <b>VIVO REDE</b>
+            <br>
+            <div class="form-group row">
+                <label for="n1_vivo_rede" class="col-form-label col-lg-1">N1:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n1_vivo_rede" id="n1_vivo_rede" placeholder="Sobreaviso/Coordenador" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n1_vivo_rede" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n1_vivo_rede" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <label for="n2_vivo_rede" class="col-form-label col-lg-1">N2:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n2_vivo_rede" id="n2_vivo_rede" placeholder="Gerencia Seção" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n2_vivo_rede" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n2_vivo_rede" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <label for="n3_vivo_rede" class="col-form-label col-lg-1">N3:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n3_vivo_rede" id="n3_vivo_rede" placeholder="Gerencia Sênior" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n3_vivo_rede" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n3_vivo_rede" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <label for="n4_vivo_rede" class="col-form-label col-lg-1">N4:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n4_vivo_rede" id="n4_vivo_rede" placeholder="Diretor" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n4_vivo_rede" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n4_vivo_rede" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <label for="n5_vivo_rede" class="col-form-label col-lg-1">N5:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n5_vivo_rede" id="n5_vivo_rede" placeholder="N5: se autorizado">
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n5_vivo_rede">
+                <select class="custom-select col-lg-2" name="forma_contato_n5_vivo_rede">
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="my-2">
+            <b>VIVO CIRE</b>
+            <br>
+            <div class="form-group row">
+                <label for="n1_vivo_cire" class="col-form-label col-lg-1">N1:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n1_vivo_cire" id="n1_vivo_cire" placeholder="Sobreaviso/Coordenador" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n1_vivo_cire" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n1_vivo_cire" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <label for="n2_vivo_cire" class="col-form-label col-lg-1">N2:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n2_vivo_cire" id="n2_vivo_cire" placeholder="Gerencia Seção" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n2_vivo_cire" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n2_vivo_cire" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <label for="n3_vivo_cire" class="col-form-label col-lg-1">N3:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n3_vivo_cire" id="n3_vivo_cire" placeholder="Gerencia Sênior" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n3_vivo_cire" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n3_vivo_cire" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <label for="n4_vivo_cire" class="col-form-label col-lg-1">N4:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n4_vivo_cire" id="n4_vivo_cire" placeholder="Diretor" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n4_vivo_cire" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n4_vivo_cire" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <label for="n5_vivo_cire" class="col-form-label col-lg-1">N5:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n5_vivo_cire" id="n5_vivo_cire" placeholder="N5: se autorizado">
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n5_vivo_cire">
+                <select class="custom-select col-lg-2" name="forma_contato_n5_vivo_cire">
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="my-2">
+            <b>EPS</b>
+            <br>
+            <div class="form-group row">
+                <label for="n1_eps" class="col-form-label col-lg-1">N1:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n1_eps" id="n1_eps" placeholder="N1 EPS" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n1_eps" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n1_eps" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <label for="n2_eps" class="col-form-label col-lg-1">N2:</label>
+                <input type="text" class="form-control mr-lg-1 col-lg-4" name="n2_eps" id="n2_eps" placeholder="N2 EPS" required>
+                <input type="time" class="form-control mr-lg-1 col-lg-2" name="horario_contato_n2_eps" required>
+                <select class="custom-select col-lg-2" name="forma_contato_n2_eps" required>
+                    <option disabled value="" selected>Meio</option>
+                    <option value="via telegram">Telegram</option>
+                    <option value="via caixa postal">Caixa postal</option>
+                    <option value="via fone">Fone</option>
+                    <option value="não possui caixa postal">Sem caixa postal</option>
+                </select>
+            </div>
+        </div>
+        <button type="#" id="btnEnviar" class="btn btn-success my-1">Gerar Carimbo</button>
+        <button type="reset" id="btnReset" class="btn btn-danger my-1">Limpar</button>
+    </form>
+
+</div>
