0b016c43648249f0fb18e34074744ef783c36e15
Janderson <janderson.rocha97@gmail.com>
2021-10-21 20:51:28 -0200

    Migration tabela avisos

---
+++ app/Database/Migrations/2021-10-20-222207_CreateTableCireBackboneAvisos.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTableCireBackboneAvisos extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true
+            ],
+            'nome_usuario_criacao' => [
+                'type' => 'VARCHAR',
+                'constraint' => 120
+            ],
+            'nome_usuario_edicao' => [
+                'type' => 'VARCHAR',
+                'constraint' => 120
+            ],
+            'descricao'          => [
+                'type' => 'TEXT',
+            ],
+            'fixado' => [
+                'type' => 'VARCHAR',
+                'constraint' => 3,
+                'default' => null,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'default' => null,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'default' => null,
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('cire_backbone_avisos');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('cire_backbone_avisos');
+    }
+}
