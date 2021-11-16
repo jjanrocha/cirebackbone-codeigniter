4b04708c25b2385d3da81378bc155e35386c9779
Janderson <janderson.rocha97@gmail.com>
2021-11-16 19:33:37 -0200

    Teste

--- app/Models/Aviso.php
+++ app/Models/Aviso.php
@@ -12,7 +12,7 @@ class Aviso extends Model
     protected $useAutoIncrement     = true;
     protected $insertID             = 0;
     protected $returnType           = 'array';
-    protected $useSoftDeletes       = true;
+    protected $useSoftDeletes       = false;
     protected $protectFields        = true;
     protected $allowedFields        = [
         'nome_usuario_criacao',
